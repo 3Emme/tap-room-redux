2ae62446feb5ec6df6411d20537c47344256564b
Emme Buentiempo <emmettbuentiempo@gmail.com>
2020-12-04 10:47:41 -0800

    write editing reducer tests

---
+++ src/__tests__/reducers/editing-reducer.test.js
@@ -0,0 +1,11 @@
+import editingReducer from '../../reducers/editing-reducer';
+
+describe("editingReducer", () => {
+
+  test('Should return default state if no action type is recognized', () => {
+    expect(editingReducer(false, { type: null })).toEqual(false);
+  });
+  test('Should toggle editing state to true', () => {
+    expect(editingReducer(false, { type: 'TOGGLE_EDITING' })).toEqual(true);
+  });
+});
