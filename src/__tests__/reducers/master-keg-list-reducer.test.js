b8ea308d1f1865c5facfd6ca8605634a58640482
Emme Buentiempo <emmettbuentiempo@gmail.com>
2020-12-04 12:03:48 -0800

    write master keg reducer with tests for create and delete

---
+++ src/__tests__/reducers/master-keg-list-reducer.test.js
@@ -0,0 +1,51 @@
+import masterKegListReducer from '../../reducers/master-keg-list-reducer';
+
+describe('masterKegListReducer', () => {
+
+  let action;
+  [{name: "Main Bar 1", brand: "Quality Booch", flavor: "Persimmon", price: "9", quantity: "124", id:"1" }]
+  const currentState = {
+    1: {name: "Main Bar 1", brand: "Quality Booch", flavor: "Persimmon", price: "9", quantity: "124", id:"1" },
+    2: {name: "Main Bar 2", brand: "Quality Booch", flavor: "Lemon", price: "9", quantity: "124", id:"2" }
+  }
+
+  const ticketData = {name: "Main Bar 1", brand: "Quality Booch", flavor: "Persimmon", price: "9", quantity: "124", id:"1" };
+
+  test('Should return default state if no action type is recognized', () => {
+    expect(masterKegListReducer({}, { type: null })).toEqual({});
+  });
+
+  test('Should successfully add new ticket data to masterKegList', () => {
+    const { name, location, issue, id } = ticketData;
+    action = {
+      type: 'ADD_KEG',
+      name: name,
+      brand: location,
+      flavor: issue,
+      price: price,
+      quantity: quantity,
+      id: id
+    };
+    expect(masterKegListReducer({}, action)).toEqual({
+      [id] : {
+        name: name,
+        brand: location,
+        flavor: issue,
+        price: price,
+        quantity: quantity,
+        id: id
+      }
+    });
+  });
+
+  test('Should successfully delete a keg', () => {
+    action = {
+      type: 'DELETE_KEG',
+      id: 1
+    };
+    expect(masterKegListReducer(currentState, action)).toEqual({
+      2: {name: "Main Bar 2", brand: "Quality Booch", flavor: "Lemon", price: "9", quantity: "124", id:"2" }
+    });
+  });
+
+});
