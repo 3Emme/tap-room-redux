d442c3d39aaaeb4a750b98d39fe81746f13094ad
Emme Buentiempo <emmettbuentiempo@gmail.com>
2020-12-04 09:23:33 -0800

    write tests for toggle-keg-list, passing

---
+++ src/__tests__/reducers/keg-list-visible-on-page-reducer.test.js
@@ -0,0 +1,11 @@
+import kegListVisibleReducer from '../../reducers/keg-list-visible-on-page-reducer';
+
+describe("kegListVisibleReducer", () => {
+
+  test('Should return default state if no action type is recognized', () => {
+    expect(kegListVisibleReducer(true, { type: null })).toEqual(true);
+  });
+  test('Should toggle market visibility state to true', () => {
+    expect(kegListVisibleReducer(true, { type: 'TOGGLE_KEG_LIST' })).toEqual(false);
+  });
+});
