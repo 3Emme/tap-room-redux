e41299f2530a4237785caa0399aedd34210bcc86
Emme Buentiempo <emmettbuentiempo@gmail.com>
2020-12-04 12:08:37 -0800

    correct typos in testing. all tests pass

--- src/reducers/master-keg-list-reducer.js
+++ src/reducers/master-keg-list-reducer.js
@@ -1,12 +1,12 @@
 export default (state = [{name: "Main Bar 1", brand: "Quality Booch", flavor: "Persimmon", price: "9", quantity: "124", id:"1" }], action) => {
-  const { name, location, issue, price, quantity, id } = action;
+  const { name, brand, flavor, price, quantity, id } = action;
   switch (action.type) {
   case 'ADD_KEG':
     return Object.assign({}, state, {
       [id]: {
         name: name,
-        brand: location,
-        flavor: issue,
+        brand: brand,
+        flavor: flavor,
         price: price,
         quantity: quantity,
         id: id
