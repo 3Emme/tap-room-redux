93868267a0571e81592d7826953a27eb3961dcb6
Emme Buentiempo <emmettbuentiempo@gmail.com>
2020-12-04 16:14:50 -0800

    fix typo

--- src/__tests__/reducers/form-visible-on-page-reducer.test.js
+++ src/__tests__/reducers/form-visible-on-page-reducer.test.js
@@ -1,11 +1,11 @@
-import formVisibleReducer from '../../reducers/form-visible-on-page-reducer';
+import formVisibleOnPageReducer from '../../reducers/form-visible-on-page-reducer';
 
-describe("formVisibleReducer", () => {
+describe("formVisibleOnPageReducer", () => {
 
   test('Should return default state if no action type is recognized', () => {
-    expect(formVisibleReducer(false, { type: null })).toEqual(false);
+    expect(formVisibleOnPageReducer(false, { type: null })).toEqual(false);
   });
   test('Should toggle form visibility state to true', () => {
-    expect(formVisibleReducer(false, { type: 'TOGGLE_FORM' })).toEqual(true);
+    expect(formVisibleOnPageReducer(false, { type: 'TOGGLE_FORM' })).toEqual(true);
   });
 });
--- src/__tests__/reducers/keg-list-visible-on-page-reducer.test.js
+++ src/__tests__/reducers/keg-list-visible-on-page-reducer.test.js
@@ -1,11 +1,11 @@
-import kegListVisibleReducer from '../../reducers/keg-list-visible-on-page-reducer';
+import kegListVisibleOnPageReducer from '../../reducers/keg-list-visible-on-page-reducer';
 
-describe("kegListVisibleReducer", () => {
+describe("kegListVisibleOnPageReducer", () => {
 
   test('Should return default state if no action type is recognized', () => {
-    expect(kegListVisibleReducer(true, { type: null })).toEqual(true);
+    expect(kegListVisibleOnPageReducer(true, { type: null })).toEqual(true);
   });
   test('Should toggle keg list visibility state to true', () => {
-    expect(kegListVisibleReducer(true, { type: 'TOGGLE_KEG_LIST' })).toEqual(false);
+    expect(kegListVisibleOnPageReducer(true, { type: 'TOGGLE_KEG_LIST' })).toEqual(false);
   });
 });
